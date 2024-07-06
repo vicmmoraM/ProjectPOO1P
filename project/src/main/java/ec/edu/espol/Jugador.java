9bc3c3deca1e6a44b4ddc38ebd341023bb94814e
Jose Andres Adrian Fierro <jadrian@espol.edu.ec>
2024-07-06 18:51:36 -0500

    constructor jugador y función baraja inicial

--- project/src/main/java/ec/edu/espol/Jugador.java
+++ project/src/main/java/ec/edu/espol/Jugador.java
@@ -3,6 +3,12 @@ import java.util.ArrayList;
 public class Jugador {
     private String nombre;
     private ArrayList<Carta> baraja;
+
+    public Jugador(String nombre){
+        this.nombre = nombre;
+        this.barajaJugador = new ArrayList<>();
+    }
+    
     public String getNombre() {
         return nombre;
     }
@@ -15,4 +21,14 @@ public class Jugador {
     public void setBaraja(ArrayList<Carta> baraja) {
         this.baraja = baraja;
     }
+
+    public void barajaInicial(ArrayList<Carta> carta){
+        Random r = new Random();
+        for(int i = 0; i < 7; i++){
+            int indice = r.nextInt(carta.size() + 1) - 1;
+            barajaJugador.add(carta.get(indice));
+            carta.remove(indice);
+        }
+    }
+    
 }
