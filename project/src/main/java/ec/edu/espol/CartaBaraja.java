2fe87485b6e44d7d2631327c3ad8ddaa5be22a33
vicmmoraM <vicmmora@espol.edu.ec>
2024-07-06 18:05:59 -0500

    Implementación de métodos/@Adrian
    Se implementa el método:
    - generarCartas
    Creacion de constructor CartaBaraja

--- project/src/main/java/ec/edu/espol/CartaBaraja.java
+++ project/src/main/java/ec/edu/espol/CartaBaraja.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 public class CartaBaraja {
     private ArrayList<Carta> cartas;
 
+    public CartaBaraja(){
+        this.cartas = new ArrayList<>();
+    }
+
     public ArrayList<Carta> getCartas() {
         return cartas;
     }
@@ -11,6 +15,28 @@ public class CartaBaraja {
     public void setCartas(ArrayList<Carta> cartas) {
         this.cartas = cartas;
     }
-
+    /*
+     * 
+     * @Adrian ggs
+     */
+    public void generarCartas(){
+        for(Colores c: Colores.values()){ //Recorriendo la clase Colores<Enum>
+            if(c != Colores.N){
+                for(int i = 0; i<=9; i++){
+                    cartas.add(new CartaNormal(c,i));
+                }
+                cartas.add(new CartaComodin(c,"^"));
+                cartas.add(new CartaComodin(c,"^"));
+                cartas.add(new CartaComodin(c,"&"));
+                cartas.add(new CartaComodin(c,"&"));
+            }
+            cartas.add(new ComodinEspecial(c, "+2"));
+            cartas.add(new ComodinEspecial(c, "+2"));
+            cartas.add(new ComodinEspecial(c, "+4"));
+            cartas.add(new ComodinEspecial(c, "+4"));
+            cartas.add(new ComodinEspecial(c, "%"));
+            cartas.add(new ComodinEspecial(c, "%"));
+        }
+    }
 
 }
