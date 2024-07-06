4811cdf10eb82e637a6edcf2f784d6cb89ae15c5
val28vv <148739300+val28vv@users.noreply.github.com>
2024-07-06 16:31:48 -0500

    Getters / Setters

--- project/src/main/java/ec/edu/espol/Jugador.java
+++ project/src/main/java/ec/edu/espol/Jugador.java
@@ -3,4 +3,16 @@ import java.util.ArrayList;
 public class Jugador {
     private String nombre;
     private ArrayList<Carta> baraja;
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public ArrayList<Carta> getBaraja() {
+        return baraja;
+    }
+    public void setBaraja(ArrayList<Carta> baraja) {
+        this.baraja = baraja;
+    }
 }
