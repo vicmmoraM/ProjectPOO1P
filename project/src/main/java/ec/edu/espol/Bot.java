86e2ea688cc40387deaaae905119bffb72342b27
Jose Andres Adrian Fierro <jadrian@espol.edu.ec>
2024-07-06 18:56:14 -0500

    constructor bot y función baraja inicial

--- project/src/main/java/ec/edu/espol/Bot.java
+++ project/src/main/java/ec/edu/espol/Bot.java
@@ -1,7 +1,11 @@
 package ec.edu.espol;
 import java.util.ArrayList;
 public class Bot {
-    private ArrayList<Carta> baraja;
+    private ArrayList<Carta> barajaBot;
+
+    public Bot(){
+        this.barajaBot = new ArrayList<>();
+    }
 
     public ArrayList<Carta> getBaraja() {
         return baraja;
@@ -11,5 +15,13 @@ public class Bot {
         this.baraja = baraja;
     }
 
-    
+    public void barajaInicial(ArrayList<Carta> carta){
+        Random r = new Random();
+        for(int i = 0; i < 7; i++){
+            int indice = r.nextInt(carta.size() + 1) - 1;
+            barajaBot.add(carta.get(indice));
+            carta.remove(indice);
+        }
+    }
+
 }
