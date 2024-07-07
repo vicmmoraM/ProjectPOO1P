58921e39dfe06b15cb84ac23a074c7bf551bc48e
Jose Andres Adrian Fierro <jadrian@espol.edu.ec>
2024-07-06 21:17:55 -0500

    cambios

--- project/src/main/java/ec/edu/espol/Main.java
+++ project/src/main/java/ec/edu/espol/Main.java
@@ -6,15 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         CartaBaraja baraja = new CartaBaraja();
-        Juego juego = new Juego();
         System.out.println("-------------------\nBienvenido a UNO\n-------------------");
         baraja.generarCartas();
         baraja.barajar();
-        String jugador = sc.nextLine();
-        Jugador j1 = new Jugador("Bot");
-        Jugador j2 = new Jugador(jugador);
+        Jugador j1 = Juego.agregarJugador();
+        Jugador j2 = new Jugador("Bot");
+        Juego juego = new Juego(j1,j2);
         j1.barajaInicial(baraja.getCartas());
-        System.out.println(baraja);
+        j2.barajaInicial(baraja.getCartas());
         System.out.println(j1);
         System.out.println(j2);
     }
