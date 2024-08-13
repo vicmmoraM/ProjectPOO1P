46096f93842036c67ed0cd635bf7de9f47862349
vicmmoraM <vicmmora@espol.edu.ec>
2024-08-13 01:19:23 -0500

    nya

--- project/src/main/java/ec/edu/espol/Juego.java
+++ project/src/main/java/ec/edu/espol/Juego.java
@@ -155,13 +155,33 @@ public class Juego {
     }
 
     private static Carta ComodinEspecial(Jugador j1, Bot bot, CartaBaraja mazo) {
-        Random r = new Random();
-        Colores[] colores = Colores.values();
-        Colores colorAleatorio = colores[r.nextInt(colores.length)];
-    
-        Carta cartaNueva = new CartaNormal(colorAleatorio, 10);
-        j1.getBarajaJugador().removeIf(carta -> carta instanceof ComodinEspecial);
-    
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, elige un color:");
+        System.out.println("1. Rojo");
+        System.out.println("2. Azul");
+        System.out.println("3. Verde");
+        System.out.println("4. Amarillo");
+        int opcionColor = scanner.nextInt();
+        Colores colorElegido;
+        if (opcionColor == 1) {
+            colorElegido = Colores.R;
+        } else if (opcionColor == 2) {
+            colorElegido = Colores.Z;
+        } else if (opcionColor == 3) {
+            colorElegido = Colores.V;
+        } else if (opcionColor == 4) {
+            colorElegido = Colores.A;
+        } else {
+            System.out.println("Opción inválida. Se asignará el color Rojo por defecto.");
+            colorElegido = Colores.R;
+        }
+        Carta cartaNueva = new CartaNormal(colorElegido, 10);
+        for (int i = 0; i < j1.getBarajaJugador().size(); i++) {
+            if (j1.getBarajaJugador().get(i) instanceof ComodinEspecial) {
+                j1.getBarajaJugador().remove(i);
+                i--; // Ajustar el índice después de la eliminación
+            }
+        }
         System.out.println("-------------------------");
         System.out.println("Baraja Jugador: " + j1.getBarajaJugador());
         System.out.println("-------------------------");
@@ -207,9 +227,15 @@ public class Juego {
     }  
 
     private static void ComodinEspecialBot(Bot bot, Jugador j1, CartaBaraja mazo) {
-        Colores color1 = Jugador.comodinesEspeciales(null, j1, bot, mazo, 0); // Ajustar si es necesario
-        Carta cartaNueva = new CartaNormal(color1, 10);
-        bot.getBarajaBot().removeIf(carta -> carta instanceof ComodinEspecial);
+        Colores[] colores = Colores.values();
+        Colores colorAleatorio = colores[new Random().nextInt(colores.length)];
+        Carta cartaNueva = new CartaNormal(colorAleatorio, 10);
+        for (int i = 0; i < bot.getBarajaBot().size(); i++) {
+            if (bot.getBarajaBot().get(i) instanceof ComodinEspecial) {
+                bot.getBarajaBot().remove(i);
+                i--; 
+            }
+        }
         System.out.println("Carta en el tablero: " + cartaNueva);
         System.out.println("Baraja BOT: " + bot.getBarajaBot());
     }
